2815cf7fe64936d1512d82501b88fd731027abbc
BardiTheWeird <48732117+BardiTheWeird@users.noreply.github.com>
2021-08-20 23:15:34 +0300

    Auto-generated commit [ci skip]

--- Angular/src/app/model/kanji-entry.ts
+++
@@ -1,9 +0,0 @@
-export type KanjiEntry = {
-    id : number;
-    literal : string;
-    meanings : string[];
-    // readings
-    on : string[];
-    kun : string[];
-    nanori : string[]
-}
--- Angular/src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  translationApiEndpoint: 'https://jphapi.ovh/translate/',
-  kanjiDictionaryApiEndpoint: 'https://jphapi.ovh/kanjidictionary/',
-};
